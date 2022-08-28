2b09414d9b6df2ed84bc375f8d6ebe2b85a9efda
TRK41 <Troyk@bell.net>
2022-08-28 18:21:22 -0400

    creating arrays for the script

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
