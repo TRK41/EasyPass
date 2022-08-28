178e6c9d4d6a27609f234cfd1b14c1d9d3ad923b
TRK41 <Troyk@bell.net>
2022-08-28 19:37:20 -0400

    adding prompts to code

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
-  const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const numbers = ['0','1','2','3','4','5','6','7','8','9']
+}
+
+function prompt(){
+  var length = prompt('How long do you want your password to be ?');
+  var lowercase = prompt('Would you like to include lowercase ?');
+  var uppercase = prompt('Would you like to include uppercase ?');
+  var number = prompt('Would you like to include numbers');
+  var special = prompt('Would you like to include Special Characters');
 }
 
 
