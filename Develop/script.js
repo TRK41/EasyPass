abb4803e18d8ecf823281f90d5616036801096d4
TRK41 <Troyk@bell.net>
2022-09-01 19:32:22 -0400

    building code

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
-  const lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  const uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const lowercases = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const uppercases = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   const numbers = ['0','1','2','3','4','5','6','7','8','9'];
   const spchar = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','"\"',']','^','_','`','{','|','}','~'];
+
+
 }
 
-function prompt(){
-  var length = prompt('How long do you want your password to be ?');
-  var lowercase = prompt('Would you like to include lowercase ?');
-  var uppercase = prompt('Would you like to include uppercase ?');
-  var number = prompt('Would you like to include numbers');
-  var special = prompt('Would you like to include Special Characters');
-}
+ 
+
 
 
 
@@ -23,9 +20,27 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+  let length = prompt('How long do you want your password to be ?');
+  if(length >= 8){
+    document.getElementById('password').innerHTML = length;
+  }
+  else alert("password is too short!")
+
+  var lowercase = confirm('Would you like to include lowercase ?');
+  var uppercase = confirm('Would you like to include uppercase ?');
+  var number = confirm('Would you like to include numbers');
+  var special = confirm('Would you like to include Special Characters');
+ // let lowercase
+   // if (lowercase == true) {
+     // document.getElementById('generate').innerHTML = lowercases();
+   // }
+  // if true  
+   //else
+   //while true
+  
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
